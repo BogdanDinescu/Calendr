25e215bbe5cd5a80401210e52c39992f5e6fa103
Bogdan <bogdandinescu@tutanota.com>
2021-06-02 16:04:10 +0300

    Delete the bottom navbar for redesign

--- app/src/main/java/com/bogdan/calendr/MainActivity.java
+++ app/src/main/java/com/bogdan/calendr/MainActivity.java
@@ -1,56 +1,19 @@
 package com.bogdan.calendr;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.applandeo.materialcalendarview.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Fragment agendaFragment;
-    Fragment calendarFragment;
-    Fragment active;
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
-        agendaFragment = new AgendaFragment();
-        calendarFragment = new CalendarFragment();
-        active = calendarFragment;
-        getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, active).commit();
-        
-        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.calendar:
-                        openFragment(calendarFragment);
-                        return true;
-                    case R.id.agenda:
-                        openFragment(agendaFragment);
-                        return true;
-                }
-                return false;
-            }
-        });
+        calendarView = findViewById(R.id.calendarView);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-    }
-
-    void openFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().hide(active).show(fragment).commit();
-        active = fragment;
-    }
 }
