22cc5b4635c1d80535139479163527cc509f281f
Bogdan <bogdandinescu@tutanota.com>
2021-06-05 23:52:00 +0300

    Added splash screen

--- app/src/main/java/com/bogdan/calendr/MainActivity.java
+++ app/src/main/java/com/bogdan/calendr/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Calendr);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
